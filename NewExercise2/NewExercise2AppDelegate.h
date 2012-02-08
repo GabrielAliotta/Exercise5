54fe3c290ed274f6c593cb1c3f66920394820e4c
GabrielAliotta <galiotta7@gmail.com>
2012-02-08 15:27:49 -0300

    Initial Commit

---
+++ NewExercise2/NewExercise2AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  NewExercise2AppDelegate.h
+//  NewExercise2
+//
+//  Created by Gabriel Aliotta on 2/8/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class NewExercise2ViewController;
+
+@interface NewExercise2AppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet NewExercise2ViewController *viewController;
+
+@end
---
+++ NewExercise2/NewExercise2ViewController.h
@@ -0,0 +1,15 @@
+//
+//  NewExercise2ViewController.h
+//  NewExercise2
+//
+//  Created by Gabriel Aliotta on 2/8/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewExercise2ViewController : UIViewController {
+    
+}
+
+@end
